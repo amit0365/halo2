a6f9826deb8607580a07c2759bbc7b9d7d315a6e
amit0365 <amit0365@gmail.com>
2024-04-06 02:56:20 -0400

    added finished squeezing

--- halo2_gadgets/src/poseidon.rs
+++ halo2_gadgets/src/poseidon.rs
@@ -234,6 +234,23 @@ impl<
             )?;
         }
     }
+
+    /// Transitions the sponge into its squeezing state.
+    #[allow(clippy::type_complexity)]
+    pub fn finish_squeezing(
+        mut self,
+        squeezed_cell: AssignedCell<F, F>,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<Sponge<F, PoseidonChip, S, Absorbing<PoseidonChip::Word, RATE>, D, T, RATE>, Error>
+    {   
+        let mode = Absorbing::init_with(PoseidonChip::Word::from(StateWord::from(squeezed_cell).into()));
+        Ok(Sponge {
+            chip: self.chip,
+            mode,
+            state: self.state,
+            _marker: PhantomData::default(),
+        })
+    }
 }
 
 /// A Poseidon hash function, built around a sponge.
